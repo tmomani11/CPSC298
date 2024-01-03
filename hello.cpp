af121570815d6620744808342456e7e134128e99
tmomani11 <155492669+tmomani11@users.noreply.github.com>
2024-01-02 17:35:09 -0800

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-/// @file hello.cpp
-/// @brief hello world program
-/// @author momani@chapman.edu
-#include <iostream>
-int main()
-{
-    std::cout << "Hello C++!" << std::endl;
-    return 0;
-}
