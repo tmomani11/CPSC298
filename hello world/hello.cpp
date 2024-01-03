fc2956bbabc6c829abba67cfceb099a7e576d83a
tmomani11 <155492669+tmomani11@users.noreply.github.com>
2024-01-02 17:38:49 -0800

    Add files via upload

---
+++ hello world/hello.cpp
@@ -0,0 +1,9 @@
+/// @file hello.cpp
+/// @brief hello world program
+/// @author momani@chapman.edu
+#include <iostream>
+int main()
+{
+    std::cout << "Hello C++!" << std::endl;
+    return 0;
+}
